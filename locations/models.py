f56ae1f5d37f85dc88d16c00ce5ea6a47ffc3bc9
Alex <wraithmaster10@protonmail.com>
2021-01-08 15:21:29 +1300

    added suggested pr changes

--- locations/models.py
+++ locations/models.py
@@ -31,13 +31,29 @@ class LocationPageTag(TaggedItemBase):
         on_delete=models.CASCADE
     )
 
+
+
+class LocationPromotion(models.Model):
+    '''
+    Location Promotion Data - has M2M relationship with LocationPage
+    '''
+    promotion_type=models.CharField(max_length=1) #one letter char, treat like enum
+    
+
+
+
 class LocationPage(Page):
+    '''
+    Location Page Data - M2M relationship with LocationPromotion
+    '''
     date = models.DateField("Creation date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
     tags = ClusterTaggableManager(through=LocationPageTag, blank=True)
-    lat_long = models.CharField(max_length=20, default='0,0')
-
+    lat_long = models.CharField(max_length=25, default='0,0') 
+    latitude=models.FloatField(blank=True)
+    longitude=models.FloatField(blank=True)
+    promotions=models.ManyToManyField(LocationPromotion)
     def main_image(self):
         gallery_item = self.gallery_images.first()
         if gallery_item:
@@ -54,12 +70,25 @@ class LocationPage(Page):
         MultiFieldPanel([
             FieldPanel('date'),
             FieldPanel('tags'),
-			FieldPanel('lat_long'),
+            FieldPanel('lat_long'),
         ], heading="Location information"),
         FieldPanel('intro'),
         FieldPanel('body'),
         InlinePanel('gallery_images', label="Gallery images"),
     ]
+
+    def save(self, *args, **kwargs):
+        self.latitude=round(float(self.lat_long.split(",")[0]),6)
+        self.longitude=round(float(self.lat_long.split(",")[1]),6)
+        super(LocationPage, self).save(*args, **kwargs)
+
+
+    def __str__(self):
+        return "date | {0} into | {1} body| {2} tags | {3} lat_long | {4} latitude | {5} longitude | {6} body | {7} promotions | {8}".format(self.date,self.intro,self.body,self.tags,self.lat_long,self.latitude,self.longitude,self.promotions)
+
+
+    def __repr__(self):
+        return "date | {0} into | {1} body| {2} tags | {3} lat_long | {4} latitude | {5} longitude | {6} body | {7} promotions | {8}".format(self.date,self.intro,self.body,self.tags,self.lat_long,self.latitude,self.longitude,self.promotions)
 
 class LocationPageGalleryImage(Orderable):
     page = ParentalKey(LocationPage, on_delete=models.CASCADE, related_name='gallery_images')
@@ -85,3 +114,4 @@ class LocationTagIndexPage(Page):
         context = super().get_context(request)
         context['locationpages'] = locationpages
         return context
+
