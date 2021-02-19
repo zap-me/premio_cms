9837ef0258fc2b4af953856099eb0329c84ff1d0
Alex <wraithmaster10@protonmail.com>
2021-02-19 13:52:46 +1300

    removed pwa stuff + added autoplay for slides

--- premio_cms/settings/base.py
+++ premio_cms/settings/base.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
 
     # third party
 
-    'pwa',
     'wagtail_lazyimages', 
     # ours
     'locations',
@@ -171,33 +170,3 @@ WAGTAIL_SITE_NAME = "premio_cms"
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
 
-#django-pwa
-PWA_APP_NAME = 'My App'
-PWA_APP_DESCRIPTION = "My app description"
-PWA_APP_THEME_COLOR = '#0A0302'
-PWA_APP_BACKGROUND_COLOR = '#ffffff'
-PWA_APP_DISPLAY = 'standalone'
-PWA_APP_SCOPE = '/'
-PWA_APP_ORIENTATION = 'any'
-PWA_APP_START_URL = '/'
-PWA_APP_STATUS_BAR_COLOR = 'default'
-PWA_APP_ICONS = [
-    {
-        'src': '/static/images/my_app_icon.png',
-        'sizes': '160x160'
-    }
-]
-PWA_APP_ICONS_APPLE = [
-    {
-        'src': '/static/images/my_apple_icon.png',
-        'sizes': '160x160'
-    }
-]
-PWA_APP_SPLASH_SCREEN = [
-    {
-        'src': '/static/images/icons/splash-640x1136.png',
-        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
-    }
-]
-PWA_APP_DIR = 'ltr'
-PWA_APP_LANG = 'en-US'
