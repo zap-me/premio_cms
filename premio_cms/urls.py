f705f3005eaf7949acbb3633eba080a7a48cedb1
4855062503 <72790952+4855062503@users.noreply.github.com>
2021-02-19 14:58:39 +1300

    removed pwa ref

--- premio_cms/urls.py
+++ premio_cms/urls.py
@@ -14,8 +14,7 @@ urlpatterns = [
     path('admin/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
 
-    path('search/', search_views.search, name='search'),
-    path('', include('pwa.urls'))
+    path('search/', search_views.search, name='search')
 
 ]
 
